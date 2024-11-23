76437bf95dd2875b0614ded8298b856760cf9402
ngoc2383 <ngockhanhmai1102@gmail.com>
2024-11-23 18:17:22 -0500

    update index.html and styles.css

---
+++ script.js
@@ -0,0 +1,11 @@
+const cat = document.getElementById('catsound');
+const dog = document.getElementById('dogsound');
+const wolf = document.getElementById('wolfsound');
+const bird = document.getElementById('birdsound');
+const chicken = document.getElementById('chickensound');
+const lion = document.getElementById('lionsound');
+
+function catSound() {
+    if ElementInternals
+    cat.play();
+}
