c9fb4c1d1ea2614e8bbe234298d69713bfe23ae9
ngoc2383 <ngockhanhmai1102@gmail.com>
2024-11-23 22:16:02 -0500

    update index.html and script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Function to play sound based on the image clicked
 function playSound(id) {
     // Select the audio element by using the id with "sound" suffix
-    const audioElement = document.getElementById(id + 'sound');
+    const audioElement = document.getElementById(id + 'Sound');
+    print(audioElement)
     
     // Check if the audio element exists, and play it if it does
     if (audioElement) {
