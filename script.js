3024a8155f8cb12fd3ad53c971feee74aa123e71
ngoc2383 <ngockhanhmai1102@gmail.com>
2024-11-24 02:07:50 -0500

    update script.js and styles.css

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Function to play sound based on the image clicked
+let currentAudio = null;
 function playSound(animal) {
     // Define the corresponding audio element for each animal
     const audioMap = {
@@ -11,13 +12,21 @@ function playSound(animal) {
     };
 
     // Get the audio element by id based on the provided animal
-    const audioElement = document.getElementById(audioMap[animal]);
+    const newAudioElement = document.getElementById(audioMap[animal]);
+
+    // If there is already an audio playing, pause it
+    if (currentAudio && currentAudio !== newAudioElement) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0; // Reset the audio to the start
+    }
 
     // If the audio element exists, play it
-    if (audioElement) {
-        audioElement.play();
+    if (newAudioElement) {
+        newAudioElement.play();
+        currentAudio = newAudioElement;
     } else {
         console.error('Audio element not found for', animal);
     }
 }
+
 
