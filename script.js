8181aaf826aee672e5c45d377947e8aec1b25fe0
ngoc2383 <ngockhanhmai1102@gmail.com>
2024-11-23 20:43:24 -0500

    update index.html and script.js

--- script.js
+++ script.js
@@ -5,7 +5,21 @@ const bird = document.getElementById('birdsound');
 const chicken = document.getElementById('chickensound');
 const lion = document.getElementById('lionsound');
 
-function catSound() {
-    if ElementInternals 
-    cat.play();
+function playSound(id) {
+    if (id == 'cat') {
+        cat.play();
+    } elif (id == 'dog') {
+        dog.play();
+    } elif (id == 'chicken'){
+        chicken.play();
+    } elif ( id == 'wolf'){
+        wolf.play();
+    } elif (id == 'lion'){
+        lion.play();
+    } elif ( id == 'bird'){
+        bird.play();
+    } else {
+        console.error('Audio element not found!')
+    }
+    
 }
